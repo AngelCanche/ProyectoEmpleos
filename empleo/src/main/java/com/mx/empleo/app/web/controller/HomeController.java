16dd6319f054420b8fa5afdcfe2d6fcb8e7f36e2
Angel Canche <angel14poot@gmail.com>
2022-10-05 15:11:26 -0500

    se agrega el proyecto Empleos

---
+++ empleo/src/main/java/com/mx/empleo/app/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.mx.empleo.app.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String mostrarHome() {
+		return "home";
+	}
+}
